828d78d20738b4444f7b3b7e74747aae2b4e8de9
MissLuckyBubble <qvkatadlg9@gmail.com>
2023-03-11 11:26:38 +0200

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,24 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+    <link rel="stylesheet" href="/app.css">
+
+</head>
+
+<body>
+        <article>
+            <h1>
+                <?=$post->title?>
+            </h1>
+            <div>
+                <?=$post->body?>
+            </div>
+        </article>
+        <a href="/">Go back</a>
+</body>
+
+</html>
